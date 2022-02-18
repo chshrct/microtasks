bf06bb70188d83bc9b6c9620493138e55bebeb27
Yauheni <eugene.kostroma@gmail.com>
2022-02-18 12:34:09 +0400

    microtask-button

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {MouseEvent} from 'react';
 import './App.css';
+import {Button} from "./components/Button";
 
 function App() {
+
+    const buttonFoo = (name:string,age:number) => {
+        console.log(name,age)
+    }
+    const stupidButtonHello = ()=>console.log('im stupid button')
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button name={'YoutubeChannel1'}
+              callback={()=>buttonFoo('Vasiliy',21)}/>
+      <Button name={'YoutubeChannel2'}
+              callback={()=>buttonFoo('Aleksey',36)}/>
+        <Button name={'+'} callback={stupidButtonHello}/>
     </div>
   );
 }
---
+++ src/components/Button.tsx
@@ -0,0 +1,14 @@
+type ButtonPropsType = {
+    name: string
+    callback: () => void
+}
+
+
+export const Button = (props: ButtonPropsType) => {
+
+    const onClickHandler = () => {
+        props.callback()
+    }
+
+    return <button onClick={onClickHandler}>{props.name}</button>
+}
